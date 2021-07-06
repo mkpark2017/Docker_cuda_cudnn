ceb5917a53032eaec0e2d081c7e7ce3c854aa05a
mkpark2017 <mk.park@unist.ac.kr>
2021-07-06 22:21:25 +0900

    upload

---
+++ CUDA10.1/run.sh
@@ -0,0 +1,4 @@
+docker run --runtime=nvidia -it --rm mk:cuda10.1-ubuntu18.04 bash
+
+#docker run --runtime=nvidia -it --name mk_gym mk:cuda10.1-ubuntu18.04 bash
+
--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run --runtime=nvidia -it --rm mk:cuda10.1-ubuntu18.04 bash
